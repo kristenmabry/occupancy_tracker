839cfd27da2799cd16c31821b802e3caed52758d
michaelwilliams258 <michaelwilliams258@u.boisestate.edu>
2022-02-08 18:33:49 -0700

    Large Changes to get ble and sensor working together, custom service added for tracking occupancy

---
+++ pca10040/s132/ses/Output/Debug/Obj/ble_app_hts_pca10040_s132/sensorsim.d
@@ -0,0 +1,5 @@
+Output/Debug/Obj/ble_app_hts_pca10040_s132/sensorsim.o: \
+ F:\Downloads\nrf_sdk\nRF5_SDK_17.1.0_ddde560\components\libraries\sensorsim\sensorsim.c \
+ F:\Downloads\nrf_sdk\nRF5_SDK_17.1.0_ddde560\components\libraries\sensorsim\sensorsim.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.64/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.64/include/stdbool.h
